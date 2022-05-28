de3c5e9af793b364894a5cfa68bf81975b810af1
subhanrein <106023376+Rein171@users.noreply.github.com>
2022-05-28 19:36:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 include_once("portofoliohome.html");
 include_once("portofolioabout.html");
+include_once("portofolip.html");
+include_once("portofolioteam.html");
+include_once("portofolioblog.html");
