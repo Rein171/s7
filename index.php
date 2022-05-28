6df0253d87776a30e181168daf45efc412a522bd
subhanrein <106023376+Rein171@users.noreply.github.com>
2022-05-28 19:22:50 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,2 @@
+<?php
+include_once("portofoliohome.html", "portofolioabout.html", "portofolip.html", "portofolioteam.html", "portofolioblog.html", "stylehome.css", "styleabout.css", "stylep", "styleteam", "styleblog");
